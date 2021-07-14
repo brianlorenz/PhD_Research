d1cee95b3918164dff87d9d92cd559fdf40b9eda
brianlorenz <brian@elorenz.com>
2021-07-13 19:29:18 -0600

    Removed *1000, it's now scaled appropriately

--- mosdef_code/prospector_code/prospector_composite_params.py
+++ mosdef_code/prospector_code/prospector_composite_params.py
@@ -69,7 +69,7 @@ run_params = {'verbose': True,
               'nested_bootstrap': 0,
               'nested_dlogz_init': 0.05,
               'nested_weight_kwargs': {"pfrac": 1.0},
-              'nested_stop_kwargs': {"post_thresh": 0.1},
+              'nested_stop_kwargs': {"post_thresh": 0.05},
               # Obs data parameters
               'objid': 0,
               'zred': 0.0,
