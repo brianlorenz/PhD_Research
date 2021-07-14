32f471a9b5bff10d02f2968317300f756cec51a1
brianlorenz <brian@elorenz.com>
2021-07-14 10:56:39 -0600

    Added plot to show stacked and scaled composites

--- mosdef_code/prospector_code/convert_flux_to_maggies.py
+++ mosdef_code/prospector_code/convert_flux_to_maggies.py
@@ -2,6 +2,7 @@
 # '/Users/brianlorenz/mosdef/composite_sed_csvs/0_sed.csv'
 
 import os
+from astropy.utils import data
 import numpy as np
 import pandas as pd
 from astropy.io import ascii
@@ -9,6 +10,7 @@ from read_data import mosdef_df
 from mosdef_obj_data_funcs import get_mosdef_obj, read_sed
 import initialize_mosdef_dirs as imd
 from scipy import interpolate
+import matplotlib.pyplot as plt
 
 
 
@@ -122,3 +124,25 @@ def prospector_maggies_to_flux_spec(spec_wave, spec):
 
 
 
+def plot_scaled_composites(n_clusters):
+    """Using the scaling that was done above, plot the scaled composite seds
+    
+    Parameters:
+    n_clusters (int): Number of clusters
+    """
+    fig, ax = plt.subplots(figsize=(8, 7))
+    for groupID in range(n_clusters):
+        data_df = ascii.read(imd.composite_sed_csvs_dir + f'/{groupID}_sed.csv').to_pandas()
+        if len(os.listdir(imd.cluster_dir + f'/{groupID}')) < 15:
+            continue
+        ax.plot(data_df['rest_wavelength'], data_df['f_lambda_scaled'], ls='-', marker='None')
+        # ax.plot([5000, 5000], [-10, 10], ls='--', color='black')
+    ax.set_xscale('log')
+    ax.set_xlim(800, 45000)
+    ax.set_ylim(-1e-16, 9e-16)
+    ax.set_xlabel('Rest Wavelength ($\AA$)', fontsize=14)
+    ax.set_ylabel('Normalized Flux', fontsize=14)
+    ax.tick_params(size=12)
+    fig.savefig(imd.composite_sed_images_dir + '/scaled_composites.pdf')
+
+# plot_scaled_composites(29)
