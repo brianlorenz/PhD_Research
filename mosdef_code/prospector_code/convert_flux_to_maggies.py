25976780d33c3eb13b869558aeb375ac30feb4c3
brianlorenz <brian@elorenz.com>
2021-07-14 11:00:56 -0600

    Plots now in lambda f_lambda

--- mosdef_code/prospector_code/convert_flux_to_maggies.py
+++ mosdef_code/prospector_code/convert_flux_to_maggies.py
@@ -135,14 +135,14 @@ def plot_scaled_composites(n_clusters):
         data_df = ascii.read(imd.composite_sed_csvs_dir + f'/{groupID}_sed.csv').to_pandas()
         if len(os.listdir(imd.cluster_dir + f'/{groupID}')) < 15:
             continue
-        ax.plot(data_df['rest_wavelength'], data_df['f_lambda_scaled'], ls='-', marker='None')
+        ax.plot(data_df['rest_wavelength'], data_df['rest_wavelength']*data_df['f_lambda_scaled'], ls='-', marker='None')
         # ax.plot([5000, 5000], [-10, 10], ls='--', color='black')
     ax.set_xscale('log')
     ax.set_xlim(800, 45000)
-    ax.set_ylim(-1e-16, 9e-16)
+    # ax.set_ylim(-1e-16, 9e-16)
     ax.set_xlabel('Rest Wavelength ($\AA$)', fontsize=14)
-    ax.set_ylabel('Normalized Flux', fontsize=14)
+    ax.set_ylabel('Normalized Flux ($\lambda$ F$_\lambda$)', fontsize=14)
     ax.tick_params(size=12)
     fig.savefig(imd.composite_sed_images_dir + '/scaled_composites.pdf')
 
-# plot_scaled_composites(29)
+plot_scaled_composites(29)
