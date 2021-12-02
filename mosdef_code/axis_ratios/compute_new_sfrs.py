3336117be97b25b83c8d8385cd31407bfd002e2c
brianlorenz <brian@elorenz.com>
2021-12-02 15:45:10 -0600

    Fixed issue where errors where zero in fit_emissio

--- mosdef_code/axis_ratios/compute_new_sfrs.py
+++ mosdef_code/axis_ratios/compute_new_sfrs.py
@@ -26,7 +26,7 @@ def convert_ha_to_sfr():
     intrinsic_halpha_lums = correct_ha_lum_for_dust(halpha_lums, avs_balmer_6565)
 
     # Derive SFR from Kennicutt 1998
-    halpha_sfrs = ha_lum_to_sfr(intrinsic_halpha_lums, imf='Chabrier')
+    halpha_sfrs = ha_lum_to_sfr(intrinsic_halpha_lums, imf='Hao_Chabrier')
 
     # Append the new sfrs to the catalog
     # Save the catalog
@@ -45,7 +45,7 @@ def read_ha_and_av():
 
 def correct_av_for_dust(avs_stellar):
     """Find the formula in Reddy et al 2015"""
-    avs_balmer = avs_stellar + 0
+    avs_balmer = avs_stellar
     return avs_balmer
 
 def apply_dust_law(avs_balmer, target_wave=6565, R_V=4.05):
@@ -95,6 +95,10 @@ def ha_lum_to_sfr(intrinsic_halpha_lums, imf='Chabrier'):
     """
     if imf == 'Chabrier':
         sfr = 1.2 * 10**(-41) * intrinsic_halpha_lums
+    if imf == 'Hao_Chabrier':  ## Hao et al 2013
+        sfr = 10**(-41.056) * intrinsic_halpha_lums
+    if imf == 'Salpeter':
+        sfr = 7.9 * 10**(-42) * intrinsic_halpha_lums
     return sfr
 
 
@@ -111,8 +115,11 @@ def plot_sfrs():
     
     fig, ax = plt.subplots(figsize=(8,8))
 
+    high_sfrs_bad =  ar_df[hb_flag_filt]['sfr'] > 100
+
     ax.plot(ar_df[~hb_flag_filt]['sfr'], ar_df[~hb_flag_filt]['halpha_sfrs'], color='black', marker='o', ls='None', label = 'H$_\\beta$ > 3 sigma')
-    #  ax.plot(ar_df[hb_flag_filt]['sfr'], ar_df[hb_flag_filt]['halpha_sfrs'], color='orange', marker='o', ls='None', label = 'H$_\\beta$ < 3 sigma')
+    ax.plot(ar_df[hb_flag_filt]['sfr'], ar_df[hb_flag_filt]['halpha_sfrs'], color='orange', marker='o', ls='None', label = 'H$_\\beta$ < 3 sigma')
+    ax.plot(ar_df[hb_flag_filt][high_sfrs_bad]['sfr'], ar_df[hb_flag_filt][high_sfrs_bad]['halpha_sfrs'], color='mediumseagreen', marker='o', ls='None', label = 'H$_\\beta$ < 3 sigma')
     ax.plot((0.001, 10**5), (0.001, 10**5), ls='--', color='blue')
 
     ax.set_xscale('log')
@@ -123,12 +130,121 @@ def plot_sfrs():
     ax.set_ylabel('SFR from H$_\\alpha$', fontsize=14)
     ax.legend(fontsize=14)
     ax.tick_params(labelsize=12)
-    fig.savefig(imd.axis_output_dir + '/sfr_comparison.pdf')
+    fig.savefig(imd.axis_output_dir + '/sfr_diagnostics/sfr_comparison.pdf')
 
 
 
-# convert_ha_to_sfr()
-# plot_sfrs()
+    # Figure similar to the one in Sedona's paper
+    fig, ax = plt.subplots(figsize=(9,8))
+
+    high_sfrs_bad =  ar_df[hb_flag_filt]['sfr'] > 100
+
+    filter_999s = np.logical_and(ar_df[~hb_flag_filt]['halpha_sfrs'] > 0, ar_df[~hb_flag_filt]['sfr'] > 0)
+
+    sfr_ratio = ar_df[~hb_flag_filt][filter_999s]['halpha_sfrs'] / ar_df[~hb_flag_filt][filter_999s]['sfr']
+
+    color_map = plt.cm.get_cmap('RdYlBu') 
+    reversed_color_map = color_map.reversed() 
+    sc = ax.scatter(ar_df[~hb_flag_filt][filter_999s]['log_mass'], np.log10(sfr_ratio) , c=ar_df[~hb_flag_filt][filter_999s]['AV'], cmap = reversed_color_map, edgecolors='black', vmin=0, vmax=1.4)
+    cbar = plt.colorbar(sc)
+    ax.plot((8, 12), (0, 0), ls='--', color='black')
+
+    ax.set_xlabel('Stellar Mass', fontsize=14)
+    ax.set_xlim(8.7, 11.6)
+    ax.set_ylim(-2.2, 1.3)
+    
+    ax.set_ylabel('log10(SFR H$_\\alpha$ / SFR Balmer)', fontsize=14)
+    cbar.set_label('FAST AV', fontsize=14)
+    ax.tick_params(labelsize=12)
+    fig.savefig(imd.axis_output_dir + '/sfr_diagnostics/sfr_comparison_vsmass.pdf')
+
+    
+
+    fig, ax = plt.subplots(figsize=(8,8))
+
+    bins = np.arange(0, 500, 25)
+
+    
+
+    ax.hist(ar_df[~hb_flag_filt]['sfr'], bins=bins, color='black', label = 'H$_\\beta$ > 3 sigma')
+    ax.hist(ar_df[hb_flag_filt]['sfr'], bins=bins, color='orange', alpha=0.5, label = 'H$_\\beta$ < 3 sigma')
+    ax.hist(ar_df[hb_flag_filt][high_sfrs_bad]['sfr'], bins=bins, color='mediumseagreen', alpha=1, label = 'H$_\\beta$ < 3 sigma')
+    
+    
+    ax.set_xlabel('SFR2', fontsize=14)
+    ax.set_ylabel('Count', fontsize=14)
+    ax.legend(fontsize=14)
+    ax.tick_params(labelsize=12)
+    fig.savefig(imd.axis_output_dir + '/sfr_diagnostics/sfr2_hist.pdf')
+
+
+    ### DIagnostic plots for why sfr is so highsfr
+
+
+    fig, ax = plt.subplots(figsize=(8,8))
+
+    remove_999s_good = np.logical_and(ar_df[~hb_flag_filt]['err_hb_flux']>-999, ar_df[~hb_flag_filt]['hb_flux']>-999)
+    remove_999s_bad = np.logical_and(ar_df[hb_flag_filt]['err_hb_flux']>-999, ar_df[hb_flag_filt]['hb_flux']>-999)
+
+    ax.plot(ar_df[~hb_flag_filt][remove_999s_good]['err_hb_flux'], ar_df[~hb_flag_filt][remove_999s_good]['hb_flux'], color='black', marker='o', ls='None', label = 'H$_\\beta$ > 3 sigma')
+    ax.plot(ar_df[hb_flag_filt][remove_999s_bad]['err_hb_flux'], ar_df[hb_flag_filt][remove_999s_bad]['hb_flux'], color='orange', marker='o', ls='None', label = 'H$_\\beta$ < 3 sigma')
+    ax.plot(ar_df[hb_flag_filt][high_sfrs_bad]['err_hb_flux'], ar_df[hb_flag_filt][high_sfrs_bad]['hb_flux'], color='mediumseagreen', marker='o', ls='None', label = 'H$_\\beta$ < 3 sigma')
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+    ax.plot((0.001*10**-18, 10**5), (0.003*10**-18, 3*10**5), ls='--', color='blue', label='Flux = 3*error')
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+
+    ax.set_xlabel('Hbeta flux error', fontsize=14)
+    ax.set_ylabel('Hbeta flux', fontsize=14)
+    ax.legend(fontsize=14)
+    ax.tick_params(labelsize=12)
+    fig.savefig(imd.axis_output_dir + '/sfr_diagnostics/sfr_hbflux_hbflxuerr.pdf')
+
+    fig, ax = plt.subplots(figsize=(8,8))
+
+    remove_999s_good = np.logical_and(ar_df[~hb_flag_filt]['err_ha_flux']>-999, ar_df[~hb_flag_filt]['ha_flux']>-999)
+    remove_999s_bad = np.logical_and(ar_df[hb_flag_filt]['err_ha_flux']>-999, ar_df[hb_flag_filt]['ha_flux']>-999)
+
+    ax.plot(ar_df[~hb_flag_filt][remove_999s_good]['err_ha_flux'], ar_df[~hb_flag_filt][remove_999s_good]['ha_flux'], color='black', marker='o', ls='None', label = 'H$_\\beta$ > 3 sigma')
+    ax.plot(ar_df[hb_flag_filt][remove_999s_bad]['err_ha_flux'], ar_df[hb_flag_filt][remove_999s_bad]['ha_flux'], color='orange', marker='o', ls='None', label = 'H$_\\beta$ < 3 sigma')
+    ax.plot(ar_df[hb_flag_filt][high_sfrs_bad]['err_ha_flux'], ar_df[hb_flag_filt][high_sfrs_bad]['ha_flux'], color='mediumseagreen', marker='o', ls='None', label = 'H$_\\beta$ < 3 sigma')
+
+    ax.set_xlabel('Halpha flux error', fontsize=14)
+    ax.set_ylabel('Halpha flux', fontsize=14)
+    ax.legend(fontsize=14)
+    ax.tick_params(labelsize=12)
+    ax.set_xlim(-0.1*10**(-17), 4*10**(-17))
+    fig.savefig(imd.axis_output_dir + '/sfr_diagnostics/sfr_haflux_haflxuerr.pdf')
+
+
+
+    fig, ax = plt.subplots(figsize=(8,8))
+
+    remove_999s_good = np.logical_and(ar_df[~hb_flag_filt]['ha_flux']>-999, ar_df[~hb_flag_filt]['hb_flux']>-999)
+    remove_999s_bad = np.logical_and(ar_df[hb_flag_filt]['ha_flux']>-999, ar_df[hb_flag_filt]['hb_flux']>-999)
+
+    ax.plot(ar_df[~hb_flag_filt][remove_999s_good]['ha_flux'], ar_df[~hb_flag_filt][remove_999s_good]['hb_flux'], color='black', marker='o', ls='None', label = 'H$_\\beta$ > 3 sigma')
+    ax.plot(ar_df[hb_flag_filt][remove_999s_bad]['ha_flux'], ar_df[hb_flag_filt][remove_999s_bad]['hb_flux'], color='orange', marker='o', ls='None', label = 'H$_\\beta$ < 3 sigma')
+    ax.plot(ar_df[hb_flag_filt][high_sfrs_bad]['ha_flux'], ar_df[hb_flag_filt][high_sfrs_bad]['hb_flux'], color='mediumseagreen', marker='o', ls='None', label = 'H$_\\beta$ < 3 sigma')
+
+    # 2.86 theory line
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+    ax.plot((0, 2.86*10**-(15)), (0, 1*10**(-15)), ls='--', color='blue', label='Balmer dec = 2.86')
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+
+    ax.set_xlabel('Halpha flux', fontsize=14)
+    ax.set_ylabel('Hbeta flux', fontsize=14)
+    ax.legend(fontsize=14)
+    ax.tick_params(labelsize=12)
+    fig.savefig(imd.axis_output_dir + '/sfr_diagnostics/sfr_haflux_hbflux.pdf')
+
+
+
+convert_ha_to_sfr()
+plot_sfrs()
 
 
 
